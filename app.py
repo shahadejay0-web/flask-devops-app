06868b1ec95795be4f3894e796eee50068744159
root <root@ip-172-31-0-120.ec2.internal>
2026-05-22 18:58:58 +0000

    Initial DevOps project

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template
-from prometheus_flask_exporter import PrometheusMetrics
+from flask import Flask, render_template, Response
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
-metrics = PrometheusMetrics(app)
+REQUEST_COUNT = Counter(
+    'app_requests_total',
+    'Total App Request Count'
+)
+
+@app.before_request
+def before_request():
+    REQUEST_COUNT.inc()
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype='text/plain')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
