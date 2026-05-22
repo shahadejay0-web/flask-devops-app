b437893d19873fbcbe81cd8a117191382187f5d1
root <root@ip-172-31-0-120.ec2.internal>
2026-05-22 15:02:42 +0000

    Initial DevOps project

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+
+metrics = PrometheusMetrics(app)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
